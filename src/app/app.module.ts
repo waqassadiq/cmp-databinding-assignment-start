81b34f0a1f6e1b0c18cd44f4e5ad62d3ef716d21
Waqas <wakassadik@gmail.com>
2021-05-07 14:45:24 +0500

    child even component created of gameControl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { GameControlComponent } from './game-control/game-control.component';
 import { OddComponent } from './game-control/odd/odd.component';
+import { EvenComponent } from './game-control/even/even.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameControlComponent,
-    OddComponent
+    OddComponent,
+    EvenComponent
   ],
   imports: [
     BrowserModule,
